e1b800fd828f43969932a45c637735bc7d554cb0
hang <dinghang@ipplus360.com>
2020-06-10 15:27:02 +0800

    :更新实例代码，优化使用体验

--- test.c
+++ test.c
@@ -29,8 +29,8 @@ awdb_strndup(const char *str, size_t n)
 int main(int argc, char **argv)
 {
     
-    char *filename = "IP_city_2020W16_single_WGS84.awdb"; //请正确填写文件路径 ，或者像埃文科技索要数据文件
-    char *ip_address ="1.192.90.183";
+    char *filename = "awdb2.awdb"; //请正确填写文件路径 ，或者像埃文科技索要数据文件
+    char *ip_address ="166.111.4.100";
     AWDB_s awdb;
     int status = AWDB_open(filename, AWDB_MODE_MMAP, &awdb);
 
@@ -63,7 +63,8 @@ if (entry_data.has_data) {
  
 status =AWDB_get_value(&result.entry, &entry_data,"accuracy",  NULL);
 if (entry_data.has_data) { 
-	char* buf=awdb_strndup((char*)entry_data.utf8_string,entry_data.data_size);
+
+	buf=awdb_strndup((char*)entry_data.utf8_string,entry_data.data_size);
 	printf("精度:\t%s\n",buf);
  }
  
@@ -71,30 +72,53 @@ if (entry_data.has_data) {
  
 status =AWDB_get_value(&result.entry, &entry_data,"zipcode",  NULL);
 if (entry_data.has_data) { 
-	char* buf=awdb_strndup((char*)entry_data.utf8_string,entry_data.data_size);
+	buf=awdb_strndup((char*)entry_data.utf8_string,entry_data.data_size);
 	printf("zipcode:\t%s\n",buf);
  }
  
  
 status =AWDB_get_value(&result.entry, &entry_data,"timezone",  NULL);
 if (entry_data.has_data) { 
-	char* buf=awdb_strndup((char*)entry_data.utf8_string,entry_data.data_size);
+	buf=awdb_strndup((char*)entry_data.utf8_string,entry_data.data_size);
 	printf("时区:\t%s\n",buf);
  }
  
- 
- status =AWDB_get_value(&result.entry, &entry_data,"prov",  NULL);
+ //可能有多个 省市  0 获取第一个
+ status =AWDB_get_value(&result.entry, &entry_data,"multiAreas","0","prov",  NULL);
 if (entry_data.has_data) { 
-	char* buf=awdb_strndup((char*)entry_data.utf8_string,entry_data.data_size);
+	buf=awdb_strndup((char*)entry_data.utf8_string,entry_data.data_size);
 	printf("省:\t%s\n",buf);
  }
  
- 
- status =AWDB_get_value(&result.entry, &entry_data,"city",  NULL);
+  
+status =AWDB_get_value(&result.entry, &entry_data,"multiAreas","0","district",  NULL);
 if (entry_data.has_data) { 
-	char* buf=awdb_strndup((char*)entry_data.utf8_string,entry_data.data_size);
-	printf("城市:\t%s\n",buf);
+
+
+	buf=awdb_strndup((char*)entry_data.utf8_string,entry_data.data_size);
+	printf("区县:\t%s\n",buf);
  }
+ 
+ status =AWDB_get_value(&result.entry, &entry_data,"multiAreas","0","latwgs",  NULL);
+if (entry_data.has_data) { 
+
+
+	buf=awdb_strndup((char*)entry_data.utf8_string,entry_data.data_size);
+	printf("纬度:\t%s\n",buf);
+ }
+  status =AWDB_get_value(&result.entry, &entry_data,"multiAreas","0","lngwgs",  NULL);
+if (entry_data.has_data) { 
+
+
+	buf=awdb_strndup((char*)entry_data.utf8_string,entry_data.data_size);
+	printf("经度:\t%s\n",buf);
+ }
+ //打印全部信息
+AWDB_entry_data_list_s *entry_data_list = NULL;
+AWDB_entry_s entry ={ .awdb = &awdb, .offset = result.entry.offset };
+status = AWDB_get_entry_data_list(&entry, &entry_data_list);
+													  
+AWDB_dump_entry_data_list(stdout, entry_data_list, 2);
  
 }
 
